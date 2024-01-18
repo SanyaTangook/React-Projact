9b5f7b291d38cf56c6671c1490c5becccc2dcdf2
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-01-19 00:22:11 +0700

    Update Button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Header } from "./func-components";
 import {Contrnt , Footer} from "./Contrnt";
 import Calendar from './class-components';
+import Button from './class-components2';
+
 function App() {
 	return (
 		<>
 			<Header />
 			<center> <Calendar /> </center> 
 			<Contrnt/>	
+			<center> <Button /> </center>
+			<br/>
+			<br/>
 			<Footer />
 		</>
 
