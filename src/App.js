9edba0bd05cdfa2bb7ddaf70509aafcd9cc7a7b3
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-01-19 00:30:48 +0700

    Update Evrnt Data

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { Header } from "./func-components";
 import {Contrnt , Footer} from "./Contrnt";
 import Calendar from './class-components';
 import Button from './class-components2';
+import { EventData } from "./event-data";
 import {Calculator} from './calculator';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<center> <Calendar /> </center> 
 			<Contrnt/>	
-			<center> <Button /> </center>
 			<br/>
 			<br/>
-			<Calculator />
+			<EventData />
 			<br/>
 			<br/>
 			<Footer />
