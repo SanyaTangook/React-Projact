100bb3579cf1e247308196a92239fadb3577ca98
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-01-19 14:06:16 +0700

    add refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from "react";
-import { Header } from "./func-components";
-import Calendar from './class-components'
-import {Contrnt , Footer} from "./Contrnt";
-import { EventData } from "./event-data";
-import {Calculator} from './calculator';
-import { Table } from "./event-data2";
-import Button from './class-components2'
+import RefsFunc from './refs-func';
+
 function App() {
 	return (
 		<>
-		<Header/>
-		<br/>
-		<p><center><Calendar/></center></p>
-		<br/>
-		<Contrnt />
-		<br/><hr/>
-		<h2 style={{textAlign:"center"}}>Button Alert</h2>
-		<br/>
-		<center><Button /></center>
-		<br/><hr/><br/>
-		<h2 style={{textAlign:"center"}}>Button Calculator</h2><br/>
-		<Calculator/>
-		<br/><hr/>
-		<h2 style={{textAlign:"center"}}>EventData Button</h2><br/>
-		<EventData/>
-		<br/><hr/>
-		<h2 style={{textAlign:"center"}}>EventData Table</h2><br/>
-		<Table/>
-		<br/>
-		<hr/>
-		<Footer/>
+			<RefsFunc />
 		</>
 	)
 }
