5882d972890d7ec8dfdff20c75fefd8a8946bb80
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-01-29 10:20:37 +0700

    Edit and Add component  Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import MessageBox from "./compoents/state-func";
+import { userContext } from "./compoents/context";
+import Content from "./compoents/context-content";
 function App() {
-	return (
-		<>
-			<MessageBox/>
-		</>
-	)
+  return (
+    <>
+      <userContext.Provider value={"Tom Jerry"}>
+        <Content />
+      </userContext.Provider>
+    </>
+  );
 }
 
 export default App;
