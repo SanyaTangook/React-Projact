f15223fe8391b16fda72e900eeccc452539d6b95
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-03-04 11:16:14 +0700

    Form hook

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import FormCheck from "./compoents/form-check";
-
+import FormValidation from "./compoents/form-validation";
 export default function App() {
   return (
-    <FormCheck />
-  )
+    <>
+      <FormCheck />
+      <FormValidation />
+    </>
+  );
 }
-
